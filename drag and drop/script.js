53266874e9579feacb56059be73b464d1fb6c89d
Vishal Rajput <99860031+rpVishal@users.noreply.github.com>
2023-06-08 17:47:41 +0530

    Add files via upload

--- drag and drop/script.js
+++ drag and drop/script.js
@@ -1,17 +1,19 @@
 const list = document.getElementById('list');
 const droppable = document.getElementById('droppable');
 const reset = document.getElementById('reset');
+//adding events for the drag and drop control
 
+//this event is used when we start dragging
 list.addEventListener('dragstart', (event) => {
     event.dataTransfer.setData('text/plain', event.target.id);
     event.target.style.opacity = '0.5';
 });
-
+//this is used when we chooses and lift the item to be dropped
 droppable.addEventListener('dragover', (event) => {
     event.preventDefault();
     droppable.classList.add('hover');
 });
-
+//this event is called when the dragged item is dropping
 droppable.addEventListener('dragleave', (event) => {
     droppable.classList.remove('hover');
 });
@@ -25,7 +27,7 @@ droppable.addEventListener('drop', (event) => {
     item.style.opacity = '1';
     alert('Item dropped!');
 });
-
+//added event for the reset button to reset the drag and drop boxes
 reset.addEventListener('click', () => {
     droppable.innerHTML = '';
     list.innerHTML = `
